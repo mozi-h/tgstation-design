97b40734b688e8c8afe57509f893965f3b2c901d
mozi_h <die-123-321@web.de>
2021-10-17 02:19:57 +0200

    Refactor `index.html` into pug files

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var { series, src, dest, watch } = require("gulp");
+const pug = require("gulp-pug-3");
+var babel = require("gulp-babel");
 var sass = require("gulp-sass")(require("sass"));
 var sourcemaps = require("gulp-sourcemaps");
 var del = require("del");
@@ -7,11 +9,35 @@ function clean() {
 	return del("./dist");
 }
 
-function copyAll() {
-	return src(["./src/**", "!./src/scss/**"]).pipe(dest("./dist"));
+function copyPublic() {
+	return src("./src/public/**").pipe(dest("./dist"));
 }
 
-function buildStyles() {
+function buildHTML() {
+	return src("./src/pug/index.pug")
+		.pipe(sourcemaps.init())
+		.pipe(
+			pug({
+				// Your options.
+			})
+		)
+		.pipe(sourcemaps.write())
+		.pipe(dest("./dist"));
+}
+
+function buildJS() {
+	return src("./src/js/**/*.js")
+		.pipe(sourcemaps.init())
+		.pipe(
+			babel({
+				presets: ["@babel/env"],
+			})
+		)
+		.pipe(sourcemaps.write())
+		.pipe(dest("./dist/js"));
+}
+
+function buildStylesDev() {
 	return src("./src/scss/**/*.scss")
 		.pipe(sourcemaps.init())
 		.pipe(
@@ -25,12 +51,16 @@ function buildStyles() {
 
 module.exports = {
 	clean,
-	copyAll,
-	buildStyles,
+	copyPublic,
+	buildHTML,
+	buildJS,
+	buildStylesDev,
 	watch: function () {
 		clean();
-		watch(["./src/**", "!./src/scss/**"], { ignoreInitial: false }, copyAll);
-		watch("./src/scss/**/*.scss", { ignoreInitial: false }, buildStyles);
+		watch("./src/public/**", { ignoreInitial: false }, copyPublic);
+		watch("./src/pug/**/*.pug", { ignoreInitial: false }, buildHTML);
+		watch("./src/js/**/*.js", { ignoreInitial: false }, buildJS);
+		watch("./src/scss/**/*.scss", { ignoreInitial: false }, buildStylesDev);
 	},
-	default: series(clean, copyAll, buildStyles),
+	default: series(clean, copyPublic, buildStylesDev),
 };
